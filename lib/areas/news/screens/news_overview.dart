9cf84282857cb80234a93eab7f3ccab21ccd07c3
Maurice Hennig <maurice.hennig@outlook.com>
2021-02-22 21:29:20 +0100

    Fixed size of refresh indicator in news

--- lib/areas/news/screens/news_overview.dart
+++ lib/areas/news/screens/news_overview.dart
@@ -3,6 +3,7 @@ import 'package:fb4_app/areas/news/widgets/news_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:math';
 
 class NewsOverview extends StatelessWidget {
   @override
@@ -20,11 +21,20 @@ class NewsOverview extends StatelessWidget {
                 builder: (context, viewModel, child) {
                   if (viewModel.newsItems.length > 0) {
                     return CustomScrollView(slivers: [
-                      CupertinoSliverRefreshControl(
-                        onRefresh: () {
-                          return viewModel.fetchNewsItems();
-                        },
-                      ),
+                      CupertinoSliverRefreshControl(onRefresh: () {
+                        return viewModel.fetchNewsItems();
+                      }, builder: (context, mode, d1, d2, d3) {
+                        const Curve opacityCurve =
+                            Interval(0.0, 0.35, curve: Curves.easeInOut);
+                        return Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Opacity(
+                            opacity: opacityCurve.transform(min(d1 / d2, 1.0)),
+                            child: CupertinoActivityIndicator.partiallyRevealed(
+                                radius: 14.0, progress: min(d1 / d2, 1.0)),
+                          ),
+                        );
+                      }),
                       SliverToBoxAdapter(
                         child: Padding(
                             padding: const EdgeInsets.all(8.0),
