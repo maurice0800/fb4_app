916834442e795bd64426c8f8edaabb80b5d95fba
Maurice Hennig <maurice.hennig@outlook.com>
2021-04-06 17:24:16 +0200

    Fixed news not showing because of bad certificate

--- lib/areas/news/repositories/news_repository.dart
+++ lib/areas/news/repositories/news_repository.dart
@@ -1,12 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:fb4_app/areas/news/models/news_item.dart';
-import 'package:http/http.dart' as http;
 import 'package:xml/xml.dart';
 
 class NewsRepository {
+  // We need to ignore https trust because FH Dortmund does not seem to care too much about using valid certificates :(
   Future<List<NewsItem>> getNewsItems() async {
-    return http.get('https://www.inf.fh-dortmund.de/rss.php').then((result) {
+    HttpClient client = HttpClient()
+      ..badCertificateCallback = (cert, host, port) {
+        return host == "www.inf.fh-dortmund.de" && port == 443;
+      };
+    return client
+        .getUrl(Uri.parse('https://www.inf.fh-dortmund.de/rss.php'))
+        .then((request) async {
+      var result = await request.close();
       if (result.statusCode == 200) {
-        var document = XmlDocument.parse(result.body);
+        var document =
+            XmlDocument.parse(await result.transform(Latin1Decoder()).join());
         var items = document.findAllElements('item');
         return items
             .map<NewsItem>((element) => NewsItem.fromXmlElement(element))
