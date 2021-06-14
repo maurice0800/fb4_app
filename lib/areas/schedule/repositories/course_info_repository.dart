3868fc1758add540dfe3a0dffe9114264e332c04
Maurice Hennig <maurice.hennig@outlook.com>
2021-06-14 19:01:29 +0200

    Added caching for course info

--- lib/areas/schedule/repositories/course_info_repository.dart
+++ lib/areas/schedule/repositories/course_info_repository.dart
@@ -4,21 +4,27 @@ import 'package:fb4_app/areas/schedule/models/course_info.dart';
 import 'package:http/http.dart' as http;
 
 class CourseInfoRepository {
+  List<CourseInfo> cachedCourses = [];
+
   Future<List<CourseInfo>> getCourses() async {
-    return http
-        .get(Uri.parse(
-            'https://ws.inf.fh-dortmund.de/fbws/current/rest/CourseOfStudy/?Accept=application/json'))
-        .then((result) {
-      if (result.statusCode == 200) {
-        final Map data = jsonDecode(result.body) as Map;
-        return data.values
-            .where((item) => item['grades'] != null)
-            .map<CourseInfo>(
-                (item) => CourseInfo.fromJson(item as Map<String, dynamic>))
-            .toList();
-      } else {
-        return [];
-      }
-    });
+    if (cachedCourses.isEmpty) {
+      return http
+          .get(Uri.parse(
+              'https://ws.inf.fh-dortmund.de/fbws/current/rest/CourseOfStudy/?Accept=application/json'))
+          .then((result) {
+        if (result.statusCode == 200) {
+          final Map data = jsonDecode(result.body) as Map;
+          return cachedCourses = data.values
+              .where((item) => item['grades'] != null)
+              .map<CourseInfo>(
+                  (item) => CourseInfo.fromJson(item as Map<String, dynamic>))
+              .toList();
+        } else {
+          return [];
+        }
+      });
+    } else {
+      return Future.value(cachedCourses);
+    }
   }
 }
